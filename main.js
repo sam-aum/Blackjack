6166614f2cc10d9ceae117738c7393425455dc88
Sam-Aum <psaum01@gmail.com>
2022-01-05 15:07:21 -0500

    MVP

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ console.log(deck)
 // console.log(newDeck)
 // let randNum = []
 let playerHand = []
+
 let houseHand = []
+
 const pHand = document.getElementById('pHand')
 const hHand = document.getElementById('hHand')
 console.log(pHand)
@@ -25,6 +27,7 @@ for (let i=0; i < suits.length; i++){
         }
         
         deck.push(card)
+        // deck.push(card.number + card.suit)
     }
 }
 
@@ -48,6 +51,7 @@ function startGame(evt){
     shuffle()
     deal()
 
+    // createPHand()
     pCardDisplay()
     hCardDisplay()
 
@@ -60,36 +64,107 @@ then create space in html where card is shown
 }
 
 function deal(){
-    let pCard1 = deck[0]
+    let pCard1 = deck.pop()
     playerHand.push(pCard1)
-    let pCard2 = deck[2]
+    let pCard2 = deck.pop()
     playerHand.push(pCard2)
-    let hCard1 = deck[1]
+    let hCard1 = deck.pop()
     houseHand.push(hCard1)
-    let hCard2 = deck[3]
+    let hCard2 = deck.pop()
     houseHand.push(hCard2)
 
     console.log(pCard1)
     console.log(pCard2)
     console.log(hCard1)
     console.log(hCard2)
+    
 }
+console.log(playerHand)
 
-
-function createCard(){
-    let card1 = playerHand[0].number + playerHand[0].suit
-}
+// function createPHand(){
+//     let card1 = playerHand[0].number + ' ' + playerHand[0].suit
+//     console.log(card1)
+// }
 
 function pCardDisplay(){
-    // pHand.innerText = card1
-
+    // for (let i=0; i< 8; i++){
+    pHand.innerText = playerHand[0].number + ' ' + playerHand[0].suit
+    pHand2.innerText = playerHand[1].number + ' ' + playerHand[1].suit
+    
+    
 }
 
 function hCardDisplay(){
-    hHand.innerText = houseHand[0].number
+    hHand.innerText = houseHand[0].number + ' ' + houseHand[0].suit
+    hHand2.innerText = houseHand[1].number + ' ' + houseHand[1].suit
 
 
 }
+
+const hitButton = document.getElementById('hit')
+console.log(hitButton)
+
+hitButton.addEventListener('click', hitPlayer)
+
+function hitPlayer(evt){
+    addCard()
+    pNewCardDisplay()
+
+}
+
+function addCard(){
+    playerHand.push(deck.pop())
+    console.log(playerHand)
+
+}
+
+function pNewCardDisplay(){
+    pHand3.innerText = playerHand[2].number + ' ' + playerHand[2].suit
+    pHand4.innerText = playerHand[3].number + ' ' + playerHand[3].suit
+    pHand5.innerText = playerHand[4].number + ' ' + playerHand[4].suit
+    pHand6.innerText = playerHand[5].number + ' ' + playerHand[5].suit
+    pHand7.innerText = playerHand[6].number + ' ' + playerHand[6].suit
+}
+
+
+const standButton = document.getElementById('stand')
+console.log(standButton)
+
+standButton.addEventListener('click', houseTurn)
+
+function houseTurn(evt){
+    
+    addHCard()
+    hNewCardDisplay()
+
+}
+
+
+function addHCard(){
+    houseHand.push(deck.pop())
+    console.log(houseHand)
+
+}
+
+function hNewCardDisplay(){
+    hHand3.innerText = houseHand[2].number + ' ' + houseHand[2].suit
+    hHand4.innerText = houseHand[3].number + ' ' + houseHand[3].suit
+    hHand5.innerText = houseHand[4].number + ' ' + houseHand[4].suit
+    hHand6.innerText = houseHand[5].number + ' ' + houseHand[5].suit
+    hHand7.innerText = houseHand[6].number + ' ' + houseHand[6].suit
+   
+
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
